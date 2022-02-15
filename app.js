19baf6cf47c3ba843e3ee44338e93f36202a6f41
AungZawMyo97 <aungzawmyo.azm1997@gmail.com>
2022-02-15 12:18:37 +0630

    Add gitignore, procfile and update ports

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const { path } = require("express/lib/application");
 const _ = require("lodash");
+const { prototype } = require("tar/lib/pax");
 
 
 
@@ -159,6 +160,11 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(prototype, function() {
+  console.log("Server started successfully!);
 });
